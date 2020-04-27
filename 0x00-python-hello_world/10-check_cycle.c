90356d722018af04c50291585c452e666c275283
VictorXRivera <victorxrivera.professional@gmail.com>
2020-04-27 21:09:08 +0000

    Set up function file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+  * check_cycle - function to check for cycle in singly linked list
+  * @list: list variable
+  * Return: 0 if no cycle, 1 if yes
+  */
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
