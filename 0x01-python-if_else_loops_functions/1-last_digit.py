8b052a7ad518a4b162a8d37cfc8095744573fe47
VictorXRivera <victorxrivera.professional@gmail.com>
2020-04-29 21:01:38 +0000

    Python script to find last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+modder = 10
+if (number < 0):
+    modder *= -1
+lastdigit = number % (modder)
 
+script = "Last digit of {:d} is {:d}".format(number, lastdigit)
+
+if lastdigit < 6 and lastdigit != 0:
+    print("{} and is less than 6 and not 0".format(script))
+elif lastdigit > 5:
+    print("{} and is greater than 5".format(script))
+elif lastdigit == 0:
+    print("{} and is 0".format(script))
