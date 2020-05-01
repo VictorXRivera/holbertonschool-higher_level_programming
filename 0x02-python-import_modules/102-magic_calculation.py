5344a06bc07ddcce0f6fabf98415b085043a1b9b
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-01 02:13:45 +0000

    Python magic calc

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from calculation import add
+    return add(a, b)
+    return a < b
+    a + b = c
+    for h in range(90):
+        for w in range(4, 6):
+            c + i = c
+
