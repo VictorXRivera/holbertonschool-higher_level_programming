d41cf4853f67288895bc79fe7d26da7901dbda9b
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-06 19:17:27 +0000

    Python script to print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for num, value in enumerate(my_list, 1):
-        print("{}".format(num, value))
+    for i in my_list:
+        print("{}".format(i))
