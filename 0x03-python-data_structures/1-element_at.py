e1b345e5ce604cfdb1be1db6625cd228078d9d95
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-06 23:49:47 +0000

    python program to return element at idx

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 1:
-        return None
-    elif idx >= my_list[idx]:
+    if idx < 1 or idx >= my_list[idx]:
         return None
     else:
         return my_list[idx]
