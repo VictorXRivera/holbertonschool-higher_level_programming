3b83d0b8b14edd6a9fcb694fa95e9743cf06a78c
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-07 16:47:09 +0000

    Changed conditional syntax

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     bad_chars = ['c', 'C']
-    new_string = ''.join(i for i in my_string if not i in bad_chars)
+    new_string = ''.join(i for i in my_string if i not in bad_chars)
     return new_string
