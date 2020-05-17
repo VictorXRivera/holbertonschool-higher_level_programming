19fd8400b23184c5621325471c77ad7505124091
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-17 21:15:35 +0000

    Rewrote conditional and loop

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,8 +3,8 @@ def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
         return None
-    for char in sentence[0]:
-        if char == 0:
+    for char in range(length):
+        if sentence[char] == 0:
             return None
         else:
-            return length, char
+            return length, sentence[char]
