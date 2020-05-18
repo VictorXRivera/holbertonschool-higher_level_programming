f1203454ef5cb134da33b9af3faac30784c107c1
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-18 21:04:15 +0000

    Function that doesn't run at a certain point

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    for i in my_list:
-        print("{:d}".format(my_list[i]), end="")
-    if x < 0:
-        return None
-    else:
-        return x
     try:
-        safe_print_list_integers()
-    except IndexError:
+        x = int(x)
+        return x
+        for i in my_list:
+            print("{}".format(my_list[i - 1]))
+    except ValueError:
         return
