69231c38078989d4fa79c43244941eed450f348b
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-19 23:12:16 +0000

    Try/Except blocks in making square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Writing class Square that defines a square by using attribute, try/except"""
+
+
+class Square:
+    """Class Square that defines a square using an private attribute.
+
+    Attributes:
+        size: private attribute to define square with
+    """
+    def __init__(self, size=0):
+        """Intializing size as private attribute as well as insanisation of attribute
+        Args:
+            size: attribute
+        """
+        # Private attribute
+        try:
+            if isinstance(size, int):
+                self.__size = size
+        except TypeError:
+            print("size must be an integer")
+        else:
+            if int(size) >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
