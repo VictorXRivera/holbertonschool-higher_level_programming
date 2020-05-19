ae6f660c11a93ea560bbe964759870baec416eb8
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-19 01:30:10 +0000

    Divide integers and print result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    answer = None
     try:
         answer = a / b
-        print("Inside result: {}".format(answer))
+        return answer
     except ZeroDivisionError:
         return None
     finally:
-        return a / b
+        print("Inside result: {}".format(answer))
