7da5901e0ec5bdcac3ab236848f3be7838cdb8ea
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-19 20:54:33 +0000

    Define square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Writing class Square that defines a square by using attribute"""
+
+
+class Square:
+    """Class Square that defines a square using an private attribute.
+
+    Attributes:
+        size: private attribute to define square with
+    """
+    def __init__(self, size):
+        """Intializing size as private attribute as well as insanisation of attribute
+        Args:
+            size: attribute
+        """
+        #:Private attribute
+        self.__size = size
