3f99bc5384bfcd8eaf47779d599d6bb173fbc664
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-20 14:32:50 +0000

    Safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        value = int(value)
         print("{:d}".format(value))
-        return True
-    except ValueError:
-            try:
-                value = float(value)
-                print("{:f}".format(value))
-                return True
-            except ValueError:
-                return False
+    except:
+        return False
+    return True
