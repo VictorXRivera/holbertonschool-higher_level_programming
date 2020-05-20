6b2306dd73e24157fa337d065d72f01307475d32
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-20 14:24:19 +0000

    Safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    print(*my_list, sep='')
-    if x < 0:
-        return None
-    else:
-        return x
-    try:
-        safe_print_list()
-    except:
-        return
+    element = 0
+    for num in range(0, x):
+        try:
+            print("{}".format(my_list[num]), end="")
+            element = element + 1
+        except:
+            pass
+    print("")
+    return element
