3e0cedc5197ce4a74a83e8df261c1b84665dc382
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-24 16:22:11 +0000

    Function to return number of keys in dict using recursion

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    return (0 if not isinstance(a_dictionary, dict)
+    else len(a_dictionary) + sum(number_keys(value) for value in a_dictionary.values()))
