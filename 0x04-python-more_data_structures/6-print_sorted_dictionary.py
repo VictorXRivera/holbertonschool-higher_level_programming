244460db661fc2e75f38c9a7f87299ae12e4a201
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-24 16:54:13 +0000

    Used sorted and str.format

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for element in sorted(a_dictionary.items()):
-        print(element[0], ":", element[1])
+    for element in sorted(a_dictionary.keys()):
+        print("{}: {}".format(element, a_dictionary[element]))
