1c0f9ed9e2b12f358ddb52a8ecb376918862256b
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-26 12:13:44 +0000

    Swapped around the instantiation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,24 +15,8 @@ class Rectangle:
         self.__height = height
     
     @property
-    def width(self):
-        """Getter for width
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Setter with edge cases
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
     def height(self):
-        """Getter for height
+        """Getter for width
         """
         return self.__height
 
@@ -41,7 +25,23 @@ class Rectangle:
         """Setter with edge cases
         """
         if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """Getter for height
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter with edge cases
+        """
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self.__width = value
