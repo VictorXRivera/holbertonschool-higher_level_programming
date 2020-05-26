65f00f9605cce29bbd930fbfc9c133ba6ded8d77
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-26 13:34:04 +0000

    Rewrote comments

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,11 @@
 class Rectangle:
     """Defining Rectangle"""
     def __init__(self, width=0, height=0):
-        """Instantiation of width and height with edge cases"""
+        """Instantiation of width and height with edge cases
+        Attributes:
+            width: private instance attribute
+            height: private instance attribute
+        """
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height < 0:
@@ -18,18 +22,6 @@ class Rectangle:
         self.__width = width
     
     @property
-    def width(self):
-        return self.__width
-    
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-    
-    @property
     def height(self):
         return self.__height
     
@@ -40,3 +32,15 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+    
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
