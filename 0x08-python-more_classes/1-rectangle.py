af6e674d2451a171d08fc591e1719c323aa9c14d
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-26 13:00:06 +0000

    Swapped getter, setter and private instance attributes around

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,21 +6,9 @@ class Rectangle:
     """Instantiation of Rectangle"""
     def __init__(self, width=0, height=0):
         # Private instance attribute
-        self.__width = width
-        # Private instance attribute
         self.__height = height
-    
-    @property
-    def width(self):
-        return self.__width
-    
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        # Private instance attribute
+        self.__width = width
     
     @property
     def height(self):
@@ -29,7 +17,19 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__height = value
+    
+    @property
+    def width(self):
+        return self.__height
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
