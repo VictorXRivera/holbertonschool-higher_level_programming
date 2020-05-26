957843947dd3b3e0890ef2061d50ab76c25578b7
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-26 13:49:52 +0000

    Added corrected instantiation and comments

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
 """Rectangle class"""
 
-
 class Rectangle:
-    """
-    defining a rectangle
+    """Defining Rectangle
     """
     def __init__(self, width=0, height=0):
-        """instantiation of width and height
+        """Instantiation of width and height with edge cases
+        Attributes:
+            width: private instance attribute
+            height: private instance attribute
         """
-        # Private attri. width
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
-        # Private attri. height
         self.__height = height
 
     def area(self):
+        """Return area of rectangle
+        """
         return self.__width * self.__height
 
     def perimeter(self):
+        """Return perimeter of rectangle
+        """
         return (2 * self.__width) + (2 * self.__height)
 
     @property
