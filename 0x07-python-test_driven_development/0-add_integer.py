897ca91476df7953e0dd0e9eb62cac79082e8be4
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-31 19:46:46 +0000

    Add integer function

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Return the sum of two integers a and b
+Conversion of float and errors included
+"""
+
+
+
+def add_integer(a, b=98):
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
