922214c7f8d4189638799c1bfb5a614c3e220b1d
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-02 23:47:11 +0000

    3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+""" Function to check is kind of class """
 def is_kind_of_class(obj, a_class):
+    """ Checks for kind of class """
     if isinstance(obj, a_class):
         return True
     return False
