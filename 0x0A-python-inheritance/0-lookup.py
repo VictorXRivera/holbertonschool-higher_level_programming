32915edd81bff51bf29b86f318b52c6995a8d099
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-02 23:05:01 +0000

    Removed space

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-function that returns the list of 
-available attributes and methods of an object
-"""
+""" function to look for objects and methods """
+
 
 def lookup(obj):
     """ Return list of objects and methods """
