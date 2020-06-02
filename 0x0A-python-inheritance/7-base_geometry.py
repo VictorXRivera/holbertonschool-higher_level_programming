e332dde7bba468da9955b91e33d8575adfb2c10d
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-02 18:01:16 +0000

    Class Geometry with edge cases

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Empty class Geometry"""
+
+
+class BaseGeometry:
+    """Class"""
+
+    def integer_validator(self, name, value):
+        """Edge cases
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        self.value = value
+        self.name = name
+    
+    def area(self):
+        """Area"""
+        raise Exception("area() is not implemented")
