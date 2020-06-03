fd7c52607b8d31c993d2116cadc188f3b5fae629
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-03 22:04:58 +0000

    Commit

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
 """ Function that returns number of lines """
 
 
 def number_of_lines(filename=""):
     """ Number of lines """
+    number = 0
+    line = 0
     with open(filename) as f:
-        for i, l in enumerate(f):
+        for number, line in enumerate(f):
             pass
-    return i + 1
+    return number + 1
