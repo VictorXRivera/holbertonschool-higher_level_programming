918bb64ae2b8656db0aebb00fe8d3201b8940c1d
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-03 23:41:26 +0000

    To json string

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Function to convert to json string """
 import json
+
+
 def to_json_string(my_obj):
+    """ Using json.dumps """
     obj = json.dumps(my_obj, sort_keys=True)
     return obj
