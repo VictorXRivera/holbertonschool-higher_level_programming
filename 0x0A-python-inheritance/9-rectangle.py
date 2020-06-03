dd7442dee2bdd3b46d887e6f7b4374f334f4e8d9
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-03 02:28:01 +0000

    Return and print area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Empty class Geometry """
+
+
+class BaseGeometry:
+    """ Geometry Class """
+
+    def integer_validator(self, name, value):
+        """ Edge cases """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        self.value = value
+        self.name = name
+    
+class Rectangle(BaseGeometry):
+    """ Rectangle Class """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return self.__width * self.__height
