1468887e54d3f33ac85d2b7d0bdd3cafd0a4ff28
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-03 01:27:14 +0000

    Rewrote conditions

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,5 +13,7 @@ class BaseGeometry:
             """ Integer validation """
             if not isinstance(value, int):
                 raise TypeError("{} must be an integer".format(name))
-            if value <= 0:
-                raise ValueError("{} must be greater than 0".format(name))
+            if value < 0:
+                raise ValueError("{} must be greater than zero".format(name))
+            if value == 0:
+                raise ValueError("{} must be greater than zero".format(name))
