9f3df55838929f1a4b4f09da534dedfac410eb9c
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-03 03:05:11 +0000

    SQUARE

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,4 +16,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """ Print statement """
-        return "[Rectangle] " + str(self.__size) + "/" + str(self.__size)
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
