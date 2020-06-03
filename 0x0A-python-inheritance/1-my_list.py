3682ae13436dba5a9e9bcb416b890d71c5845c99
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-03 00:50:42 +0000

    Added check for type

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,4 +7,5 @@ class MyList(list):
     def print_sorted(self):
         """ Print sorted """
         my_list = list(self)
+        all(isinstance(i, int) for i in my_list)
         print(sorted(my_list))
