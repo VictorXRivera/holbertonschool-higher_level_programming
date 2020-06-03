ef7bafba184eba2395b0c74f893c46d441298b77
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-03 00:55:07 +0000

    Commit

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""Empty class Geometry"""
+""" Empty class Geometry """
 
 
 class BaseGeometry:
-    """Class"""
+    """ Class """
 
     def integer_validator(self, name, value):
-        """Edge cases
-        """
+        """ Edge cases """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        self.value = value
-        self.name = name
     
     def area(self):
         """Area"""
