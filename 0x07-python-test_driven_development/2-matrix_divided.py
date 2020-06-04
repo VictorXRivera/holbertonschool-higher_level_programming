d20c380b41913f5536530ef94b213b78f61c4abc
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-04 14:17:04 +0000

    Commit

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,24 +6,37 @@ def matrix_divided(matrix, div):
     """ Returning a divided matrix """
     div_matrix = []
 
-    if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
 
     for element in matrix:
-        if type(element) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if not matrix:
+            raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
 
-        for ele2 in element:
-            if type(ele2) not in [int, float]:
-                raise TypeError("matrix must be a matrix (lists of lists) of integers/float")
+        if type(element) is not list:
+            raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
+
+        for ele in element:
+            if not element:
+                raise TypeError("matrix must be matrix \
+(list of lists) of integers/floats")
+
+            if type(ele) not in [int, float]:
+                raise TypeError("matrix must be a \
+matrix (list of lists) of integers/floats")
 
         if len(element) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix must \
+have the same size")
+        
         if type(div) not in [int, float]:
             raise TypeError("div must be a number")
         if div == 0:
             raise ZeroDivisionError("division by zero")
 
-    div_matrix = [[round(ele2 / div, 2) for ele2 in element] for element in matrix]
+    div_matrix = [[round(ele / div, 2) for ele in element] for element in matrix]
 
     return div_matrix
