d698257329959748832921a7d882ede627c5165a
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-04 01:40:07 +0000

    Student 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Student Class """
+
+
+class Student:
+    """ Instantiation """
+    def __init__(self, first_name, last_name, age):
+        """ Attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Return json str """
+        if attrs is None:
+            return self.__dict__
+        if not isinstance(attrs, list):
+            return self.__dict__
+        for index in attrs:
+            if not isinstance(index, str):
+                return self.__dict__
+        dictionary = {}
+        for index in attrs:
+            try:
+                dictionary[index] = self.__dict__[index]
+            except:
+                pass
+        return dictionary
