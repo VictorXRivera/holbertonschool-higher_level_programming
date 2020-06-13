7c04050a0c8d4a5e7f8bbd91ef98494328174754
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-13 17:35:25 +0000

    Complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for (l, e) in dict(a_dictionary).items():
+        if v is value:
+            a_dictionary.pop(x, None)
+    return a_dictionary
