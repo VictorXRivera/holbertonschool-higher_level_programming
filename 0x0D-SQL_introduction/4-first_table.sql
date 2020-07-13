12742e8ae9619b77c400d5e11f06018618b667ce
VictorXRivera <victorxrivera.professional@gmail.com>
2020-07-13 17:29:51 +0000

    Script to create first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,6 @@
 -- Script to create my first table
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT,
-	name VARCHAR(256) NOT NULL,
+CREATE TABLE IF NOT EXISTS first_table
+(
+id int,
+name varchar(256)
 );
