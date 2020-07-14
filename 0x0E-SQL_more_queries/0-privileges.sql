1a7b3c4852893176cb8c383b11ad84f95cf77bff
VictorXRivera <victorxrivera.professional@gmail.com>
2020-07-14 22:47:09 +0000

    Create read user Script

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Script that lists all privileges of the MySQL users user_0d_1 
 -- and user_0d_2 on my server (in localhost)
-SHOW GRANTS FOR user_od_1@localhost;
-SHOW GRANTS FOR user_od_2@localhost;
+SHOW GRANTS FOR user_od_1@`localhost`;
+SHOW GRANTS FOR user_od_2@`localhost`;
