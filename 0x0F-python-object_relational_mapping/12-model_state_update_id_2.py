e9a2aa0e4e18a09fcb9247463a66c3e982926b35
VictorXRivera <victorxrivera.professional@gmail.com>
2020-08-09 08:40:29 +0000

    Update states by id

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects from database hbtn_0e_6_usa
+"""
+if __name__ == "__main__":
+    """ Importing modules """
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State
+    """ Variables representing arguments """
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    """ Start engine """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, db_name))
+    """ Create a configured class Session """
+    Session = sessionmaker(bind=engine)
+    """ Setting up session """
+    my_session = Session()
+    """ Changing name of object with 2 as it's id """
+    state_to_update = my_session.query(State).filter(State.id == 2).one()
+    state_to_update.name = 'New Mexico'
+    """ Commit new state """
+    my_session.commit()
+    """ Session closed """
+    my_session.close()
