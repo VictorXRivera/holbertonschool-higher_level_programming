60f717bcb4cdccdbffaf3748937c817a9cb48e45
VictorXRivera <victorxrivera.professional@gmail.com>
2020-08-18 23:58:04 +0000

    Changed code around

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Curl a JSON file with POST request
-curl -s "$1" -X POST -H "Content-Type: application/json" -d "$(cat "$2")
+curl $1 -sX POST -H "Content-Type: application/json" -d @$2
