1746c42acf5e22c700adf323b0202f316d4b418c
VictorXRivera <victorxrivera.professional@gmail.com>
2020-08-24 23:55:58 +0000

    Error code script

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Easier Error request script """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r is not None:
+        if r.status_code == requests.codes.ok:
+            print(r.text)
+        else:
+            print('Error code: {}'.format(resp.status_code))
