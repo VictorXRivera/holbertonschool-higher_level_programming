97a04702c5761f52952d72d615b782fe0c80b76e
VictorXRivera <victorxrivera.professional@gmail.com>
2020-08-25 00:47:15 +0000

    JSON script

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-""" JSON API """
-import sys
 import requests
+import sys
+
 
 if __name__ == "__main__":
-    if len(argv) < 2 or not argv[1].isalpha():
-        print("No result")
-    else:
-        value = {'q': argv[1]}
-        r = requests.post('http://0.0.0.0:5000/search_user', data=value)
-        try:
-            id_json = r.json().get('id')
-            name_json = r.json().get('name')
-            print('[{}] {}'.format(id_json, name_json))
-        except:
-            print("Not a valid JSON")
+    json_dict = {}
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) == 1:
+        json_dict['q'] = ""
+    elif len(sys.argv) == 2:
+        json_dict['q'] = sys.argv[1]
+
+    r = requests.post(url, json_dict)
+    try:
+        json_rep = r.json()
+        if bool(json_rep) is False:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_rep['id'], json_rep['name']))
+    except:
+        print("Not a valid JSON")
