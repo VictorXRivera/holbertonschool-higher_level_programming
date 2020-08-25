ec79355f3d4c751b96990a4093eb1bc147c9e933
VictorXRivera <victorxrivera.professional@gmail.com>
2020-08-25 00:00:50 +0000

    Error code script

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
         if r.status_code == requests.codes.ok:
             print(r.text)
         else:
-            print('Error code: {}'.format(resp.status_code))
+            print('Error code: {}'.format(r.status_code))
