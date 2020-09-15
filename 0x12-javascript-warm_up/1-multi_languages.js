4c4593ab4dc471e390450614e08c96a20b4f65df
VictorXRivera <victorxrivera.professional@gmail.com>
2020-09-15 19:58:58 +0000

    Multi print script

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script that prints three lines
+const Var1 = 'C is fun';
+const Var2 = 'Python is cool';
+const Var3 = 'Javascript is is amazing';
+console.log(Var1);
+console.log(Var2);
+console.log(Var3);
