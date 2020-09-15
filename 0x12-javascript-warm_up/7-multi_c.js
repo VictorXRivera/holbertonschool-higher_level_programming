d209266f07a49371a9cb237171e3a10bffa6ae0d
VictorXRivera <victorxrivera.professional@gmail.com>
2020-09-15 21:42:18 +0000

    Multi c string script

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Script that prints C is Fun x number times
+if ((isNaN(process.argv[2])) === false) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
